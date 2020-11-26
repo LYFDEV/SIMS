1a8d84ff087cfcfb091f9b4c84aa55c92120a9a7
祎凡 李 <3622495140@qq.com>
2020-11-26 19:51:47 +0800

    对象关系映射层

---
+++ SIMSModel/StdInfoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.Model
+{
+    public class StdInfoModel
+    {
+        public string StdNum { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Nation { get; set; }
+        public string E_Mail { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
