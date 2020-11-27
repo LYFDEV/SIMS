671a99bcb543d99ced64802fc583d36db5355f0a
祎凡 李 <3622495140@qq.com>
2020-11-27 22:17:54 +0800

    Revert "Revert "业务逻辑层：新建管理员和超级管理员DAL类""
    This reverts commit 85f0c6b289f796df54f1570bdbd60e400e3c3715.

---
+++ SIMSBLL/AdminInfoBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIMS.DAL;
+using SIMS.Model;
+
+namespace SIMS.BLL
+{
+    public class AdminInfoBLL
+    {
+        private AdminInfoDAL aid = new AdminInfoDAL();
+
+        //创建管理员
+        public int AddAdmin(string userName, string password)
+        {
+            return aid.AddAdmin(userName, password);
+        }
+
+        //删除管理员
+        public int DeleteAddmin(string userName)
+        {
+            return aid.DeleteAddmin(userName);
+        }
+
+        //提取管理员信息
+        public List<AdminInfoModel> GetAdminInfon()
+        {
+            return aid.GetAdminInfo();
+        }
+    }
+}
---
+++ SIMSBLL/SuperAdminBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIMS.DAL;
+using SIMS.Model;
+
+namespace SIMS.BLL
+{
+    public class SuperAdminBLL
+    {
+        private SuperAdminDAL sad = new SuperAdminDAL();
+
+        //查询超级管理员信息
+        public List<SuperAdminModel> GetSuperAdmin()
+        {
+            return sad.GetSuperAdmin();
+        }
+    }
+}
