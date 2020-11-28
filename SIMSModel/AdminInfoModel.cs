b51cb7e40b4683a17eae73bbbfd97a6d2e0d56b3
祎凡 李 <3622495140@qq.com>
2020-11-28 13:44:46 +0800

    对象关系映射层：新建管理员和超级管理员Model类

---
+++ SIMSModel/AdminInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.Model
+{
+    public class AdminInfoModel
+    {
+        public string AdminName { get; set; }
+        public string Password { get; set; }
+    }
+}
